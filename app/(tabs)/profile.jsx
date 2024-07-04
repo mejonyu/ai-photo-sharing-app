46ad76c2da6c74e58fc18882f29c3d28e4f1bbbc
Jonathan Yu <jonyu02@gmail.com>
2024-07-04 14:43:20 -0400

    Create profile page UI

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,15 +1,14 @@
-import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
-import React, { useEffect } from "react";
+import { View, FlatList, TouchableOpacity, Image } from "react-native";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import SearchInput from "../../components/SearchInput";
 import EmptyState from "../../components/EmptyState";
 import { getUserPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
-import { useLocalSearchParams } from "expo-router";
 import { useGlobalContext } from "../../context/GlobalProvider";
 import { icons } from "../../constants";
+import InfoBox from "../../components/InfoBox";
 
 const Profile = () => {
   const { user, setuser, setIsLoggedIn } = useGlobalContext();
@@ -41,6 +40,25 @@ const Profile = () => {
                 source={{ uri: user?.avatar }}
                 className="w-[90%] h-[90%] rounded-lg"
                 resizeMode="cover"
+              />
+            </View>
+
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
               />
             </View>
           </View>
